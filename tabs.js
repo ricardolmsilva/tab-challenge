26d36288b07f06ce1626a95fa8b9873b0043da03
Ricardo <ricardo@Ricardos-MacBook-Air.local>
2020-07-07 20:49:03 +0100

    onload fetch dynamic data

--- tabs.js
+++ tabs.js
@@ -1,1 +1,14 @@
-console.log('log from tabs.js');
+window.onload = function loadData() {
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((tab, index) => {
+        createTabs(tab, index);
+        createTabsContent(tab, index);
+      });
+    });
+};
+
+function createTabs(tab, index) {}
+function createTabsContent(tab, index) {}
